d596fd93edd8e84dd6bb1c4413e82103821e04b9
Shengwei LI <51034296+sw-li@users.noreply.github.com>
2023-05-06 11:20:12 +0200

    backend routes finished. Iteration #1 done

--- server/routes/phone.routes.js
+++ server/routes/phone.routes.js
@@ -2,6 +2,37 @@ const express = require("express");
 const router = express.Router();
 const Phone = require("../models/Phone.model");
 
+
+
+
+router.post("/create-phone", (req, res, next) => {
+  const { name, manufacturer, description, color, price, imageFileName, screen, processor, ram } = req.body;
+
+  if (name === "" || manufacturer === "" || description === "" || color === "" || price === "" || imageFileName === "" || screen === "" || processor === "" || ram ==="") {
+      res.status(400).json({ message: "Provide all required informations." });
+      return;
+    }
+  
+  Phone.findOne({ name })
+  .then((foundPhone) => {
+      if (foundPhone) {
+          res.status(400).json({ message: "Phone already exists." });
+          return;
+        }
+      Phone.create({ name, manufacturer, description, color, price, imageFileName, screen, processor, ram })
+      .then((createdPhone) => {
+          res.status(201).json(createdPhone);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+  })
+  .catch((err) => {
+      res.status(500).json(err);
+    } );
+  })
+
+
 router.get("/phones", (req, res, next) => {
     Phone.find()
     .then((phones) => {
@@ -16,36 +47,19 @@ router.get("/phones", (req, res, next) => {
         })
 });
 
-
-
-router.post("/create-phone", (req, res, next) => {
-    const { name, manufacturer, description, color, price, imageFileName, screen, processor, ram } = req.body;
-
-    if (name === "" || manufacturer === "" || description === "" || color === "" || price === "" || imageFileName === "" || screen === "" || processor === "" || ram ==="") {
-        res.status(400).json({ message: "Provide all required informations." });
-        return;
-      }
-    
-    Phone.findOne({ name })
-    .then((foundPhone) => {
-        if (foundPhone) {
-            res.status(400).json({ message: "Phone already exists." });
+router.get("/phone/:id" , (req, res, next) => {
+    Phone.findById(req.params.id)
+    .then((phone) => {
+        if (!phone) {
+            res.status(404).json({ message: `Phone with this id:${req.params.id} is not found.` });
             return;
           }
-        Phone.create({ name, manufacturer, description, color, price, imageFileName, screen, processor, ram })
-        .then((createdPhone) => {
-            res.status(201).json(createdPhone);
-          })
-          .catch((err) => {
-            res.status(500).json(err);
-          });
-    })
+        res.status(200).json(phone);
+        })
     .catch((err) => {
-        res.status(500).json(err);
-      } );
-    })
-
-
+        res.status(500).json(`there is an error on retriving the phone with this id: ${req.params.id}. And here is the error: `, err);
+        })
+})
 
 
 
